b3114bc3b4b4b57462fa139b5322a0c88932e592
aaron <aaron.wolf@gmail.com>
2020-04-23 21:51:31 +0300

    monster rolodex tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Yoooooo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Spinch
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  
+  constructor() {
+    super();
+
+    this.state = [
+      {
+        name: 'Boogeyman'
+      },
+      {
+        name: 'Ghost'
+      },
+      {
+        name: 'Ice Cream Man'
+      }
+    ]
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.string}! 
+            {8-7}
+          </p>
+          <button onClick={() => this.setState({string : "Hey Yall, I'm Marry Poppins"})}>Change Text</button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
