b0b7290d45573ba48d580d19087627a5e52b1392
aaron <aaron.wolf@gmail.com>
2020-04-27 10:23:35 +0300

    search field

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
-
 
 class App extends Component {
   
@@ -9,25 +8,23 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'boogeyman',
-          id: 1
-        },
-        {
-          name: 'Ice Cream Man',
-          id: 2
-        }
-      ]
+      monsters: [],
+      searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
   }
   
   render() {
     return (
-      <div className="App">
-        {this.state.monsters.map(monster => (
-            <h1 key={monster.id}> {monster.name}</h1>
-        ))}
+      <div className='App'>
+        <input type='search' placeholder='search monsters' 
+        onChange={e => this.setState({ searchField: e.target.value })} />
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
