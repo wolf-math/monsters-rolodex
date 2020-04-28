5de887e15fb0a36b1d279c683ee5cf2af2fd44f8
aaron <aaron.wolf@gmail.com>
2020-04-28 19:50:26 +0300

    Updates

---
+++ precache-manifest.22a7e82559c3b687faecc8f9498dd83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c611a12fc88897fcece52dd7b729ec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d4979fa3dda23f43ef13",
+    "url": "/monsters-rolodex/static/css/main.7ae066f1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4979fa3dda23f43ef13",
+    "url": "/monsters-rolodex/static/js/main.bee44792.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
